3334f746291b29b1810a7b8babb0ccf9a6203f10
j k <janish.khanna233@gmail.com>
2019-06-26 15:43:24 -0500

    Enabled Migrations and Added Roles

---
+++ SchedularApp/Models/Enum/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchedularApp.Models.Enum
+{
+    public enum UserRoles
+    {
+        Admin,
+        Instructor,
+        Student
+    }
+}
